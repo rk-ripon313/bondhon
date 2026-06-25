f1b26fd6834817d41aac236bc6fab02d89f20637
rk-ripon360 <rk.ripon.rkr@gmail.com>
2026-06-25 21:38:57 +0600

    refactor(lib): rename dbConnect.ts to kebab-case db-connect.ts

---
+++ lib/db/db-connect.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI is missing");
+}
+
+declare global {
+  var mongooseCache: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+}
+
+let cached = global.mongooseCache;
+
+if (!cached) {
+  cached = global.mongooseCache = {
+    conn: null,
+    promise: null,
+  };
+}
+
+export async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI);
+  }
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+}
